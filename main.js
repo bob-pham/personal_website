db1090632b762d28854c7e82969a3a27fbda27d8
Bob Pham <bobbaopham@gmail.com>
2022-05-01 00:22:21 -0700

    small changes in hopes that it will work, removed an import

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import './css/style.css'
-
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
 
